f476df472b15fa25ae4ec7c16679a4551b84305b
taturou <taturou@gmail.com>
2018-10-05 22:14:37 +0900

    世界を作って、それを表示できるようにした

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,66 @@
+use std::fmt;
+
+#[derive(Debug)]
+struct LifeGame {
+    world :Vec<bool>,
+    width: usize,
+    height: usize
+}
+
+impl LifeGame {
+    fn new(width: usize, height: usize) -> LifeGame {
+        let len = width * height;
+        let world = vec![false; len];
+
+        LifeGame {
+            world,
+            width,
+            height
+        }
+    }
+
+    fn xy2i(&self, x: usize, y: usize) -> usize {
+        (self.width * y) + x
+    }
+
+    fn get(&self, x: usize, y: usize) -> bool {
+        let i = self.xy2i(x, y);
+        self.world[i]
+    }
+    
+    fn set(&mut self, x: usize, y: usize, cell: bool) {
+        let i = self.xy2i(x, y);
+        self.world[i] = cell;
+    }
+    
+    pub fn width(&self) -> usize {
+        self.width
+    }
+    
+    pub fn height(&self) -> usize {
+        self.height
+    }
+}
+
+impl fmt::Display for LifeGame {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let summary = format!("({}, {})", self.width, self.height);
+
+        let mut world = String::new();
+        for y in 0..self.height {
+            for x in 0..self.width {
+                let cell = self.get(x, y);
+                let cell = if cell { "o " } else { "x " };
+                world.push_str(cell);
+            }
+            world.push_str("\n");
+        }
+
+        write!(f, "{}\n{}", summary, world)
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut game = LifeGame::new(5, 5);
+    println!("{}", game);
 }
