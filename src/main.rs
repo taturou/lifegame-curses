7dc867ebcab92101a38e75e8e88f9db505bb162a
taturou <taturou@gmail.com>
2018-10-09 05:08:01 +0900

    LifeGame板を表示するようにした

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,49 @@ use lifegame::*;
 use cursive::Cursive;
 use cursive::views::{Dialog, LinearLayout, Panel};
 use cursive::Printer;
+use cursive::direction::Direction;
+use cursive::vec::Vec2;
 
 struct Game {
+    game: LifeGame
 }
 
 impl Game {
     fn new() -> Game {
-        Game {}
+        let mut game = LifeGame::new(20, 10);
+        game.reset_by_rand();
+        Game {
+            game
+        }
     }
 }
 
 impl cursive::view::View for Game {
     fn draw(&self, printer: &Printer) {
+        let width = self.game.width();
+        let height = self.game.height();
+
+        for y in 0..height {
+            for x in 0..width {
+                let cell = self.game.get(x, y);
+                let text = match cell {
+                    true => "\u{25a0}",
+                    false => "\u{25a1}"
+                };
+                printer.print(((x * 2) as usize, y as usize), text);
+            }
+        }
+    }
+
+    fn take_focus(&mut self, _: Direction) -> bool {
+        true
+    }
+
+    fn required_size(&mut self, _: Vec2) -> Vec2 {
+        Vec2::new(
+            (self.game.width() * 2) as usize,
+            self.game.height() as usize
+        )
     }
 }
 
@@ -43,7 +74,7 @@ fn main() {
                 LinearLayout::horizontal()
                     .child(Panel::new(Game::new())),
             ).button("Quit game", |s| {
-                s.pop_layer();
+                s.quit();
             }),
     );
 
