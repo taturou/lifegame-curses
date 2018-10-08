e1e68055fa913ffd758fb5e39fb571e34f194e86
taturou <taturou@gmail.com>
2018-10-08 12:23:52 +0900

    Cursiveで、空のダイアログを表示するようにした

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,28 @@
+extern crate cursive;
+
 mod lifegame;
 
 use lifegame::*;
+use cursive::Cursive;
+use cursive::views::{Dialog, LinearLayout, Panel};
+use cursive::Printer;
+
+struct Game {
+}
+
+impl Game {
+    fn new() -> Game {
+        Game {}
+    }
+}
+
+impl cursive::view::View for Game {
+    fn draw(&self, printer: &Printer) {
+    }
+}
 
 fn main() {
+    /*
     let mut game = LifeGame::new(20, 10);
     println!("{}", game);
 
@@ -11,4 +31,21 @@ fn main() {
 
     game.evolution();
     println!("{}", game);
+    */
+    let mut siv = Cursive::default();
+
+    siv.add_global_callback('q', |s| s.quit());
+
+    siv.add_layer(
+        Dialog::new()
+            .title("LifeGame")
+            .content(
+                LinearLayout::horizontal()
+                    .child(Panel::new(Game::new())),
+            ).button("Quit game", |s| {
+                s.pop_layer();
+            }),
+    );
+
+    siv.run();
 }
