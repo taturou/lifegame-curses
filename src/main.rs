4bda1577dea11ad0c90594786f03939a86e5f345
taturou <taturou@gmail.com>
2018-10-11 23:28:09 +0900

    LifeGame オブジェクトを Game の外に出した
    複数 View から LifeGame オブジェクトを参照するための準備。

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ extern crate cursive;
 
 mod lifegame;
 
+use std::sync::Arc;
+use std::sync::RwLock;
 use lifegame::*;
 use cursive::traits::*;
 use cursive::Cursive;
@@ -13,12 +15,11 @@ use cursive::event::{Event, EventResult, MouseButton, MouseEvent};
 use cursive::theme::{BaseColor, Color, ColorStyle};
 
 struct Game {
-    game: LifeGame
+    game: Arc<RwLock<LifeGame>>
 }
 
 impl Game {
-    fn new(width: isize, height: isize) -> Game {
-        let game = LifeGame::new(width, height);
+    fn new(game: Arc<RwLock<LifeGame>>) -> Game {
         Game {
             game
         }
@@ -27,18 +28,20 @@ impl Game {
 
 impl cursive::view::View for Game {
     fn draw(&self, printer: &Printer) {
-        let width = self.game.width();
-        let height = self.game.height();
+        let game = self.game.read().unwrap();
+
+        let width = game.width();
+        let height = game.height();
 
         for y in 0..height {
             for x in 0..width {
-                let cell = self.game.get(x, y);
+                let cell = game.get(x, y);
                 let (text, color) = match cell {
                     true => ("o", Color::Dark(BaseColor::Red)),
                     false => (".", Color::Light(BaseColor::Black))
                 };
                 printer.with_color(
-                    ColorStyle::new(color, Color::Dark(BaseColor::White)), 
+                    ColorStyle::new(color, Color::Dark(BaseColor::White)),
                     |printer| printer.print(((x * 2) as usize, y as usize), text));
             }
         }
@@ -49,9 +52,11 @@ impl cursive::view::View for Game {
     }
 
     fn required_size(&mut self, _: Vec2) -> Vec2 {
+        let game = self.game.read().unwrap();
+
         Vec2::new(
-            (self.game.width() * 2) as usize,
-            self.game.height() as usize
+            (game.width() * 2) as usize,
+            game.height() as usize
         )
     }
 
@@ -65,12 +70,14 @@ impl cursive::view::View for Game {
                 match button {
                     MouseButton::Left => {
                         if (position.x >= offset.x) && (position.y >= offset.y) {
+                            let mut game = self.game.write().unwrap();
+
                             let x = ((position.x - offset.x) / 2) as isize;
                             let y = (position.y - offset.y) as isize;
 
-                            if (x < self.game.width()) && (y < self.game.height()) {
-                                let cell = self.game.get(x, y);
-                                self.game.set(x, y, if cell { false } else { true});
+                            if (x < game.width()) && (y < game.height()) {
+                                let cell = game.get(x, y);
+                                game.set(x, y, if cell { false } else { true});
                             }
                         }
                         return EventResult::Consumed(None);
@@ -86,24 +93,29 @@ impl cursive::view::View for Game {
 
 fn main() {
     let mut siv = Cursive::default();
-
     let screen_size = siv.screen_size();
+
+    let game = Arc::new(
+                RwLock::new(
+                    LifeGame::new(
+                        ((screen_size.x as isize) / 2) - 6,
+                        (screen_size.y as isize) - 10)));
 
     siv.add_global_callback('c', |s| {
         s.call_on_id("game", |view: &mut Game| {
-            view.game.reset();
+            view.game.write().unwrap().reset();
         });
     });
 
     siv.add_global_callback('r', |s| {
         s.call_on_id("game", |view: &mut Game| {
-            view.game.reset_by_rand();
+            view.game.write().unwrap().reset_by_rand();
         });
     });
 
     siv.add_global_callback('e', |s| {
         s.call_on_id("game", |view: &mut Game| {
-            view.game.evolution();
+            view.game.write().unwrap().evolution();
         });
     });
 
@@ -116,20 +128,19 @@ fn main() {
             .title("LifeGame")
             .content(
                 Panel::new(
-                    Game::new(((screen_size.x as isize) / 2) - 6,
-                               (screen_size.y as isize) - 10)
+                    Game::new(game.clone())
                     .with_id("game"))
             ).button("Clear", |s| {
                 s.call_on_id("game", |view: &mut Game| {
-                    view.game.reset();
+                    view.game.write().unwrap().reset();
                 });
             }).button("Random", |s| {
                 s.call_on_id("game", |view: &mut Game| {
-                    view.game.reset_by_rand();
+                    view.game.write().unwrap().reset_by_rand();
                 });
             }).button("Evolution", |s| {
                 s.call_on_id("game", |view: &mut Game| {
-                    view.game.evolution();
+                    view.game.write().unwrap().evolution();
                 });
             }).button("Quit", |s| {
                 s.quit()
